9892949f668469a53cf9401792dc8765677a22d2
eronaralves <alveseronar@gmail.com>
2024-11-12 19:31:44 -0300

    refactor: app listen port

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,10 @@
 import { app } from './app'
 import { env } from './env'
 
-app.listen(env.PORT, () => {
-  console.log('HTTP Server Running!')
-})
+app
+  .listen({
+    port: env.PORT,
+  })
+  .then(() => {
+    console.log('HTTP Server Running!')
+  })
